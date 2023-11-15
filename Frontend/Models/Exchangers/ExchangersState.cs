63ecae000a32323f2c2cce7cad8e5e9aa4fe4c0a
VoxelFun <padolod@gmail.com>
2023-11-15 22:18:57 +0100

    Add Exchangers to Frontend

---
+++ Frontend/Models/Exchangers/ExchangersState.cs
@@ -0,0 +1,12 @@
+﻿using Common.Models.Exchangers;
+using Frontend.Utils.ObjectsStates;
+
+namespace Frontend.Models.Exchangers
+{
+    public class ExchangersState : ObjectsState<ExchangerDto, long>
+    {
+        protected override string BaseUrl => "api/exchangers";
+
+        public ExchangerDto? Default => Objects.FirstOrDefault();
+    }
+}
